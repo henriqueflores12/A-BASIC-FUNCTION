f17affaf6bda303908da6be2cb1bb5ac0300d15d
fagner henrique flores <henriquefagner@hotmail.com>
2020-02-21 19:46:08 -0600

    final commit

---
+++ ABASICFUNCTION/a-basic-function.js
@@ -0,0 +1,88 @@
+var msg =
+  "Sparta was a warrior society in ancient Greece that reached the height of its power after defeating rival city-state Athens in the Peloponnesian War (431-404 B.C.). Spartan culture was centered on loyalty to the state and military service. At age 7, Spartan boys entered a rigorous state-sponsored education, military training and socialization program. Known as the Agoge, the system emphasized duty, discipline and endurance. Although Spartan women were not active in the military, they were educated and enjoyed more status and freedom than other Greek women. Because Spartan men were professional soldiers, all manual labor was done by a slave class, the Helots. Despite their military prowess, the Spartans’ dominance was short-lived: In 371 B.C., they were defeated by Thebes at the Battle of Leuctra, and their empire went into a long period of decline.";
+function upDateMessage() {
+  var el = document.getElementById("message");
+  el.textContent = msg;
+}
+upDateMessage();
+
+var text =
+  "Spartan Society Sparta, also known as Lacedaemon, was an ancient Greek city-state located primarily in the present-day region of southern Greece called Laconia. The population of Sparta consisted of three main groups: the Spartans, or Spartiates, who were full citizens; the Helots, or serfs/slaves; and the Perioeci, who were neither slaves nor citizens. The Perioeci, whose name means “dwellers-around,” worked as craftsmen and traders, and built weapons for the Spartans.All healthy male Spartan citizens participated in the compulsory state-sponsored education system, the Agoge, which emphasized obedience, endurance, courage and self-control. Spartan men devoted their lives to military service, and lived communally well into adulthood. A Spartan was taught that loyalty to the state came before everything else, including one’s family The Helots, whose name means “captives,” were fellow Greeks, originally from Laconia and Messenia, who had been conquered by the Spartans and turned into slaves. The Spartans’ way of life would not have been possible without the Helots, who handled all the day-to-day tasks and unskilled labor required to keep society functioning: They were farmers, domestic servants, nurses and military attendants Spartans, who were outnumbered by the Helots, often treated them brutally and oppressively in an effort to prevent uprisings. Spartans would humiliate the Helots by doing such things asforcing them to get debilitatingly drunk on wine and then make fools of themselves in public. This practice was also intended to demonstrate to young people how an adult Spartan should never act asself control was a prized trait Methods of mistreatment could be far more extreme Spartans were allowed to kill Helots for being too smart or too fit among other reasons.";
+function sayHello() {
+  var el = document.getElementById("one");
+  el.textContent = text;
+}
+sayHello();
+
+var say =
+  "The Spartan MilitaryUnlike such Greek city-states as Athens, a center for the arts, learning and philosophy, Sparta was centered on a warrior culture. Male Spartan citizens were allowed only one occupation: solider. Indoctrination into this lifestyle began early. Spartan boys started their military training at age 7, when they left home and entered the Agoge. The boys lived communally under austere conditions. They were subjected to continual physical, competitions (which could involve violence), given meager rations and expected to become skilled at stealing food, among other survival skills.The teenage boys who demonstrated the most leadership potential were selected for participation in the Crypteia, which acted as a secret police force whose primary goal was to terrorize the general Helot population and murder those who were troublemakers. At age 20, Spartan males became full-time soldiers, and remained on active duty until age 60.The Spartans’ constant military drilling and discipline made them skilled at the ancient Greek style of fighting in a phalanx formation. In the phalanx, the army worked as a unit in a close, deep formation, and made coordinated mass maneuvers. No one soldier was considered superior to another. Going into battle, a Spartan soldier, or hoplite, wore a large bronze helmet, breastplate and ankle guards, and carried a round shield made of bronze and wood, a long spear and sword. Spartan warriors were also known for their long hair and red cloaks.";
+function tellSomeThing() {
+  var el = document.getElementById("two");
+  el.textContent = say;
+}
+tellSomeThing();
+
+var tell =
+  "Spartan Women and Marriage Spartan women had a reputation for being independent-minded, and enjoyed more freedoms and power than their counterparts throughout ancient Greece. While they played no role in the military, female Spartans often received a formal education, although separate from boys and not at boarding schools. In part to attract mates, females engaged in athletic competitions, including javelin-throwing and wrestling, and also sang and danced competitively. As adults, Spartan women were allowed to own and manage property. Additionally, they were typically unencumbered by domestic responsibilities such as cooking, cleaning and making clothing, tasks which were handled by the helots.Marriage was important to Spartans, as the state put pressure on people to have male children who would grow up to become citizen-warriors, and replace those who died in battle. Men who delayed marriage were publically shamed, while those who fathered multiple sons could be rewardedIn preparation for marriage, Spartan women had their heads shaved; they kept their hair short after they wed. Married couples typically lived apart, as men under 30 were required to continue residing in communal barracks. In order to see their wives during this time, husbands had to sneak away at night. ";
+function doSomeThingSpartan() {
+  var el = document.getElementById("three");
+  el.textContent = tell;
+}
+doSomeThingSpartan();
+
+var something =
+  "In 371 B.C., Sparta suffered a catastrophic defeat at the hands of the Thebans at the Battle of Leuctra. In a further blow, late the following year, Thebangeneral Epaminondas (c.418 B.C.-362B.C.)led an invasion into Spartan territory and oversaw the liberation of the Messenian Helots, who had been enslaved by the Spartans for several centuries. The Spartans would continue to exist, although as a second-rate power in a long period of decline. In 1834,Otto (1815-67), the king of Greece, ordered thefounding of the modern-daytown of Spartion the site of ancient Sparta.";
+function Spartan() {
+  var el = document.getElementById("four");
+  el.textContent = something;
+}
+Spartan();
+
+var force =
+  "Leonidas (c. 530-480 B.C.) was a king of the city-state of Sparta from about 490 B.C. until his death at the Battle of Thermopylae against the Persian army in 480 B.C. Although Leonidas lost the battle, his death at Thermopylae was seen as a heroic sacrifice because he sent most of his army away when he realized that the Persians had outmaneuvered him. Three hundred of his fellow Spartans stayed with him to fight and die. Almost everything that is known about Leonidas comes from the work of the Greek historian Herodotus (c. 484-c. 425 B.C.).";
+function thing() {
+  var el = document.getElementById("five");
+  el.textContent = force;
+}
+thing();
+
+var veloz =
+  "Leonidas was the son of the Spartan king Anaxandrides (died c. 520 B.C.). He became king when his older half-brother Cleomenes I (also a son of Anaxandrides) died under violent, and slightly mysterious, circumstances in 490 B.C. without having produced a male heir.Did you know? The Thermopylae pass was also the site of two other ancient battles. In 279 B.C., Gallic forces broke through Greek forces there by using the same alternate route that the Persians did in 480 B.C. In 191 B.C., the Roman army defeated an invasion of Greece by the Syrian king Antiochus III at Thermopylae.As king, Leonidas was a military leader as well as a political one. Like all male Spartan citizens, Leonidas had been trained mentally and physically since childhood in preparation to become a hoplite warrior. Hoplites were armed with a round shield, spear and iron short sword. In battle, they used a formation called a phalanx, in which rows of hoplites stood directly next to each other so that their shields overlapped with one another. During a frontal attack, this wall of shields provided significant protection to the warriors behind it. If the phalanx broke or if the enemy attacked from the side or the rear, however, the formation became vulnerable. It was this fatal weakness to the otherwise formidable phalanx formation that proved to be Leonidas’ undoing against an invading Persian army at the Battle of Thermopylae in 480 B.C.";
+function speed() {
+  var el = document.getElementById("six");
+  el.textContent = veloz;
+}
+speed();
+
+var jump =
+  "Under Xerxes I, the Persian army moved south through Greece on the eastern coast, accompanied by the Persian navy moving parallel to the shore. To reach its destination at Attica, the region controlled by the city-state of Athens, the Persians needed to go through the coastal pass of Thermopylae (or the “Hot Gates,” so known because of nearby sulfur springs). In the late summer of 480 B.C., Leonidas led an army of 6,000 to 7,000 Greeks from many city-states, including 300 Spartans, in an attempt to prevent the Persians from passing through Thermopylae.Leonidas established his army at Thermopylae, expecting that the narrow pass would funnel the Persian army toward his own force. For two days, the Greeks withstood the determined attacks of their far more numerous enemy. Leonidas’ plan worked well at first, but he did not know that there was a route over the mountains to the west of Thermopylae that would allow the enemy to bypass his fortified position along the coast. A local Greek told Xerxes about this other route and led the Persian army across it, enabling them to surround the Greeks. Much of the Greek force retreated rather than face the Persian army. An army of Spartans, Thespians and Thebans remained to fight the Persians. Leonidas and the 300 Spartans with him were all killed, along with most of their remaining allies. The Persians found and beheaded Leonidas’ corpse–an act that was considered to be a grave insult.";
+function porrada() {
+  var el = document.getElementById("seven");
+  el.textContent = jump;
+}
+porrada();
+
+var beast =
+  "Leonidas’ sacrifice, along with that of his Spartan hoplites, did not prevent the Persians from moving down the Greek coast into Boeotia. In September 480 B.C., however, the Athenian navy defeated the Persians at the Battle of Salamis, after which the Persians returned home. Nonetheless, Leonidas’ action demonstrated Sparta’s willingness to sacrifice itself for the protection of the Greek region.Leonidas achieved lasting fame for his personal sacrifice. Hero cults were an established custom in ancient Greece from the eighth century B.C. onward. Dead heroes were worshipped, usually near their burial site, as intermediaries to the gods. Forty years after the battle, Sparta retrieved Leonidas’ remains (or what were believed to be his remains) and a shrine was built in his honor";
+function superStrong() {
+  var el = document.getElementById("eight");
+  el.textContent = beast;
+}
+superStrong();
+
+var junior =
+  "“Myth has two main functions,” the poet and scholar Robert Graves wrote in 1955. “The first is to answer the sort of awkward questions that children ask, such as ‘Who made the world? How will it end? Who was the first man? Where do souls go after death?’…The second function of myth is to justify an existing social system and account for traditional rites and customs.” In ancient Greece, stories about gods and goddesses and heroes and monsters were an important part of everyday life. They explained everything from religious rituals to the weather, and they gave meaning to the world people saw around them.";
+function neymar() {
+  var el = document.getElementById("nine");
+  el.textContent = junior;
+}
+neymar();
+console.log(junior);
+
+var Julius =
+  "Around 700 BC, the poet Hesiod’s Theogony offered the first written cosmogony, or origin story, of Greek mythology. The Theogony tells the story of the universe’s journey from nothingness (Chaos, a primeval void) to being, and details an elaborate family tree of elements, gods and goddesses who evolved from Chaos and descended from Gaia (Earth), Ouranos (Sky), Pontos (Sea) and Tartaros (the Underworld).Later Greek writers and artists used and elaborated upon these sources in their own work. For instance, mythological figures and events appear in the 5th-century plays of Aeschylus, Sophocles and Euripides and the lyric poems of Pindar. Writers such as the 2nd-century BC Greek mythographer Apollodorus of Athens and the 1st-century BC Roman historian Gaius Julius Hyginus compiled the ancient myths and legends for contemporary audiences";
+function legends() {
+  var el = document.getElementById("ten");
+  el.textContent = Julius;
+}
+legends();
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # A-BASIC-FUNCTION
+
+in this example, the user is shown a message on the page in a html element whose id attribute has a value of message
+the message is going to be changed using javascript
+
+thes statements update the message at the top of the page the function acts like a store it holds the statements that are contained in the curly braces until you are ready to use them those statements are not run untin the function is called the fucntion is only called on the last line of this script....
+
+i created a history of spartan's warriors with this basic function and some basic css work .......
+
+i like to do repetition that helps you memorize it how it works very good...
